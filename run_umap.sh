15c498d92e2d2f3ca8dfb1a5d6f779d8971f10ae
sabirdvd <iee@live.jp>
2022-12-22 08:38:43 +0100

    Update and rename run.sh to run_umap.sh

--- run_umap.sh
+++ run_umap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export CONDA_ALWAYS_YES="true"
-conda create -n AraToppython=3.7 anaconda &&
-conda activate AraTop  
+conda create -n AraTop_umap python=3.7 anaconda &&
+conda activate AraTop_umap  
 pip install bertopic &&
 pip install flair  &&
 wget https://www.dropbox.com/s/3fqno4wxyktlclu/cleaned_tweet_gen_remove_emoji_v4.csv.zip &&
